3e571f95a8203cd9a01ab16eacb80d7ce2de4317
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-04-26 20:27:16 +0530

    added: ClassProps.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css';
+import ClassProps from "./ClassProps";
 
 function App() {
   return (
     <div className="App">
       <h1> React Props</h1>
+      <ClassProps />
     </div>
   );
 }
---
+++ src/ClassProps.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ClassProps extends Component {
+  render() {
+    return (
+      <div>ClassProps</div>
+    )
+  }
+}
+
+export default ClassProps
